97e23c367d7593143b6aad498eeb2688b0cc9340
Tiago Elyan Staciaki <129956665+TiagoStaciaki@users.noreply.github.com>
2023-09-30 15:13:10 -0300

    Delete EX7.py

--- EX7.py
+++
@@ -1,8 +0,0 @@
-numero1 = int(input('Digite o primeiro numero: '))
-numero2 = int(input('Digite o segundo numero: '))
-
-if numero1 > 0 and numero2 > 0:
-    print('Ambos são positivos')
-
-else:
-    print('Um deles não é positivo')
